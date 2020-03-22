c7bd23dc6f09b0d038ee3024b2c977f1a94bc32c
Cleyson Lima <cleysonph@gmail.com>
2020-03-22 16:56:53 -0300

    Add the Genre model in the admin

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -6,3 +6,8 @@ from .models import Language, Genre, Author, Book
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'modified_at')
+
+
+@admin.register(Genre)
+class Genre(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'modified_at')
