84de5b608b4f0722f271c35226f51fa2f529223a
Cleyson Lima <cleysonph@gmail.com>
2020-03-22 16:55:46 -0300

    Add the Language model in the admin

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Language, Genre, Author, Book
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'modified_at')
