201ddf5e9e368fea2f47e9baad6f854fa00c33be
Cleyson Lima <cleysonph@gmail.com>
2020-03-22 16:40:43 -0300

    Create the Language model in bookshelf app

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -17,3 +17,23 @@ class Author(Base):
                           'thumb': {'width': 480, 'height': 480, 'crop': True}})
     birth_date = models.DateField('Nascimento', blank=True, null=True)
     death_date = models.DateField('Falecimento', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+        ordering = ('-created_at', 'name')
+
+    def __str__(self):
+        return self.name
+
+
+class Language(Base):
+    name = models.CharField('Língua', max_length=100)
+
+    class Meta:
+        verbose_name = 'Língua'
+        verbose_name_plural = 'Línguas'
+        ordering = ('-created_at', 'name')
+
+    def __str__(self):
+        return self.name
