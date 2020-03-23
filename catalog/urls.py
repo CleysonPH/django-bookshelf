01712654f0544a9df87bc3129b6455981e084e6e
Cleyson Lima <cleysonph@gmail.com>
2020-03-23 00:13:37 -0300

    Create the book_detail page

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BooksListView
+from .views import BookDetailView, BooksListView
 
 
 app_name = 'catalog'
 urlpatterns = [
     path('livros', BooksListView.as_view(), name='books-list'),
+    path('livro/<int:pk>/detalhes', BookDetailView.as_view(), name='book-detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -12,3 +12,13 @@ class BooksListView(ListView):
         context['page_title'] = 'Livros'
         context['page_description'] = 'Lista com todos os livros cadatrados na plataforma'
         return context
+
+
+class BookDetailView(DetailView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetailView, self).get_context_data(**kwargs)
+        context['page_title'] = 'Detalhes'
+        context['page_description'] = 'Todas as informações do livro'
+        return context
