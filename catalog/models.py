856979923c5e55de8758fe29211b3f34919b423e
Cleyson Lima <cleysonph@gmail.com>
2020-03-23 00:20:08 -0300

    Adjust the thumb size of the authors

--- catalog/models.py
+++ catalog/models.py
@@ -14,7 +14,7 @@ class Author(Base):
     name = models.CharField('Nome', max_length=100)
     description = models.TextField('Biografia')
     image = StdImageField('Imagem', upload_to='authors', variations={
-                          'thumb': {'width': 480, 'height': 480, 'crop': True}})
+                          'thumb': {'width': 64, 'height': 64, 'crop': True}})
     birth_date = models.DateField('Nascimento', blank=True, null=True)
     death_date = models.DateField('Falecimento', blank=True, null=True)
 
