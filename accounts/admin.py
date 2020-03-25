63b8a6824ea1dee449fcce157344ca24115b72c2
Cleyson Lima <cleysonph@gmail.com>
2020-03-25 00:52:48 -0300

    Register the UserProfile in the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',  'favorite_genre', 'created_at', 'modified_at')
