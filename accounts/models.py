8cb454608813d65fc0d2f8a8402c3f135a85028d
Cleyson Lima <cleysonph@gmail.com>
2020-03-25 00:49:37 -0300

    Create the UserProfile Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField('Data de criação', auto_now_add=True)
+    modified_at = models.DateTimeField('Ultima modificação', auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UserProfile(Base):
+    image = StdImageField('Imagem', upload_to='users', variations={
+                          'thumb': {'width': 64, 'height': 64, 'crop': True}})
+    description = models.TextField('Biografia')
+    favorite_genre = models.ForeignKey(
+        "catalog.Genre", verbose_name='Gênero favorito', on_delete=models.SET_NULL, null=True, blank=False)
+    user = models.OneToOneField(
+        "auth.User", verbose_name='Usuário', on_delete=models.CASCADE, related_name='profile')
+
+    class Meta:
+        verbose_name = 'Pefil do usuário'
+        verbose_name_plural = 'Perfis de usuários'
+
+    def __str__(self):
+        return self.user.username
