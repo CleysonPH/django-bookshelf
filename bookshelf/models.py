cf0fd516ade7b14dd18580660023edde433754a6
Cleyson Lima <cleysonph@gmail.com>
2020-03-28 16:51:46 -0300

    Create the model BookshelfItem for the boolshelf app

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField('Data de criação', auto_now_add=True)
+    modified_at = models.DateTimeField('Ultima modificação', auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BookshelfItem(Base):
+
+    STATUS_CHOICE = (
+        ('reading', 'Estou lendo'),
+        ('read', 'Já li'),
+        ('want', 'Quero ler'),
+        ('stop', 'Abandonei'),
+    )
+
+    user = models.ForeignKey(
+        'auth.User', verbose_name='Usuário', on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        'catalog.Book', verbose_name='Livro', on_delete=models.CASCADE)
+    status = models.CharField('Status', max_length=7, choices=STATUS_CHOICE)
+
+    class Meta:
+        verbose_name = 'Item da prateleira'
+        verbose_name_plural = 'Itens da prateleira'
+
+    def __str__(self):
+        return f'{self.status} {self.book.title}'
