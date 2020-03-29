3f0d1fe8696ba4f5f8f611d5d7d53d4091f55109
Cleyson Lima <cleysonph@gmail.com>
2020-03-28 23:08:04 -0300

    Implemented the buttons for change the bookshelf item status

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,7 +1,11 @@
+from bookshelf.views import mark_book_has_read, mark_book_has_reading, mark_book_has_want_read
 from django.urls import path
 
 
 app_name = 'bookshelf'
 urlpatterns = [
-
+    path('<int:pk>/lido', mark_book_has_read, name='mark-book-has-read'),
+    path('<int:pk>/lendo', mark_book_has_reading, name='mark-book-has-reading'),
+    path('<int:pk>/quer_ler', mark_book_has_want_read,
+         name='mark-book-has-want-read'),
 ]
--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from .models import BookshelfItem
+from catalog.models import Book
+
+
+def mark_book_has_read(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    try:
+        bookshelf_item = BookshelfItem.objects.get(
+            user=request.user,
+            book=book
+        )
+        bookshelf_item.status = 'read'
+        bookshelf_item.save()
+    except BookshelfItem.DoesNotExist:
+        bookshelf_item = BookshelfItem.objects.create(
+            user=request.user,
+            book=book,
+            status='read'
+        )
+
+    return redirect('catalog:book-detail', pk=book.pk)
+
+
+def mark_book_has_reading(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    try:
+        bookshelf_item = BookshelfItem.objects.get(
+            user=request.user,
+            book=book
+        )
+        bookshelf_item.status = 'reading'
+        bookshelf_item.save()
+    except BookshelfItem.DoesNotExist:
+        bookshelf_item = BookshelfItem.objects.create(
+            user=request.user,
+            book=book,
+            status='reading'
+        )
+
+    return redirect('catalog:book-detail', pk=book.pk)
+
+
+def mark_book_has_want_read(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    try:
+        bookshelf_item = BookshelfItem.objects.get(
+            user=request.user,
+            book=book
+        )
+        bookshelf_item.status = 'want'
+        bookshelf_item.save()
+    except BookshelfItem.DoesNotExist:
+        bookshelf_item = BookshelfItem.objects.create(
+            user=request.user,
+            book=book,
+            status='want'
+        )
+
+    return redirect('catalog:book-detail', pk=book.pk)
