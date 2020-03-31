11e86d1cd748ef3d78adb574de72e3154f6fd8d4
Cleyson Lima <cleysonph@gmail.com>
2020-03-31 12:21:55 -0300

    Create the model Review in the reviews app

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField('Data de criação', auto_now_add=True)
+    modified_at = models.DateTimeField('Ultima modificação', auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Review(Base):
+    title = models.CharField('Título', max_lenght=100)
+    description = models.TextField('Crítica')
+    rating = models.IntegerField('Nota', validators=[
+        MaxValueValidator(5),
+        MinValueValidator(0)
+    ])
+    book = models.ForeignKey(
+        'catalog.Book',
+        verbose_name='Livro',
+        on_delete=models.CASCADE
+    ) 
+    user = models.ForeignKey(
+        'auth.User',
+        verbose_name="Usuário",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'Avaliação',
+        verbose_name_plural = 'Avaliações'
+
+    def __str__(self):
+        return f'{self.book.title} - {self.title} - {self.rating}'
