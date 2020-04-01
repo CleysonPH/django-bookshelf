e5407d9068547e40ebd9a1aa3dd670aad90cab67
Cleyson Lima <cleysonph@gmail.com>
2020-04-01 13:41:05 -0300

    Remove not used import

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import ListView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
