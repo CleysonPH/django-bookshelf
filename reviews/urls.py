71c30f517d3766f6b9b597217cdbbda45b24abf1
Cleyson Lima <cleysonph@gmail.com>
2020-04-01 13:18:54 -0300

    Create the list reviews page

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ReviewList, review_create
+from .views import ReviewList, ReviewUpdate, review_create
 
 
 app_name = 'reviews'
 urlpatterns = [
     path('<int:book_pk>/nova/critica', review_create, name='review-create'),
-    path('criticas/', ReviewList.as_view(), name='review-list'),
+    path('criticas', ReviewList.as_view(), name='review-list'),
+    path('critica/<int:pk>/editar', ReviewUpdate.as_view(), name='review-update'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from .forms import ReviewModelForm
 from .models import Review
@@ -41,4 +42,19 @@ class ReviewList(ListView, LoginRequiredMixin):
         context = super(ReviewList, self).get_context_data(**kwargs)
         context['page_title'] = 'Críticas'
         context['page_description'] = 'Todos as críticas realizadas por você'
+        return context
+
+
+class ReviewUpdate(UpdateView, LoginRequiredMixin):
+    model = Review
+    form_class = ReviewModelForm
+    success_url = reverse_lazy('reviews:review-list')
+
+    def get_object(self):
+        return get_object_or_404(Review, pk=self.kwargs.get('pk'), user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(ReviewUpdate, self).get_context_data(**kwargs)
+        context['page_title'] = 'Editar Crítica'
+        context['page_description'] = f'Edite aqui a crítica "{context["review"].title}"'
         return context
